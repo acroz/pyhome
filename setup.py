6e4d686528a117692a5cb0e3dae839d541ba116a
Andrew Crozier <wacrozier@gmail.com>
2016-11-22 16:32:00 +0000

    Bumped version to 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyhome',
-    version='0.1.1',
+    version='0.2.0',
     description='Dotfile management and synchronisation tool',
     long_description=open('README.rst').read(),
     url='https://github.com/acroz/pyhome',
